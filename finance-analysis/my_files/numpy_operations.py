5dcb01268459064cd8773091b721b99da6aff919
oski89 <oskar.norman.0@gmail.com>
2018-12-19 20:47:35 +0100

    Course progress

---
+++ finance-analysis/my_files/numpy_operations.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+arr = np.arange(10)
+print(arr)
+print()
+
+# Elementwise operations
+print(arr + arr)
+print(arr - arr)
+print(arr * arr)
+print(arr / arr)
+print(1 / arr)
+print(arr ** 3)
+print(arr + 100)
+print(np.sqrt(arr))
+print(np.exp(arr))
+print(np.max(arr))  # same as arr.max()
+print(np.cos(arr))
